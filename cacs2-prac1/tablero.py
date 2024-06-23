9c6860d763ad08ced45922a869ed82dfdd53d59b
cacs2-ua <cacs2@alu.ua.es>
2024-06-23 19:35:26 +0200

    load board from file 1.0

--- cacs2-prac1/tablero.py
+++ cacs2-prac1/tablero.py
@@ -1,15 +1,26 @@
 # Representa el crucigrama: medidas y matriz correspondiente a las celdas
 class Tablero:    
-    def __init__(self, FILS, COLS):
-        self.ancho=COLS
-        self.alto=FILS    
-        self.tablero=[]
-        
-        for i in range(self.alto):
-            self.tablero.append([])
-            for j in range(self.ancho):
-                self.tablero[i].append('-')
+    def __init__(self, FILS=None, COLS=None, file_path=None):
+        if file_path:
+            self.load_from_file(file_path)
+        else:
+            self.ancho = COLS
+            self.alto = FILS    
+            self.tablero = [['-' for _ in range(self.ancho)] for _ in range(self.alto)]
                  
+    def load_from_file(self, file_path):
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        
+        self.alto = len(lines)
+        self.ancho = len(lines[0].strip())
+        self.tablero = []
+        
+        for line in lines:
+            self.tablero.append(list(line.strip()))
+            
+    def set_from_file(self, file_path):
+        self.load_from_file(file_path)
         
     def __str__(self):
         salida=""
